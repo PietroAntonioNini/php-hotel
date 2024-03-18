24591d1268515abc48cb3b7d24b36cc951a0bfde
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-03-18 16:50:57 +0100

    Inizio Bonus

--- index.php
+++ index.php
@@ -44,42 +44,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
 
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container mt-5">
+        <h1 class="text-uppercase text-center">PHP Hotel</h1>
+
+        <form method="GET" action="" class="mt-5 mb-5">
+            <label class="me-1" for="parking">Parcheggio:</label>
+            <input class="me-5" type="checkbox" id="parking" name="parking" value="yes">
+
+            <label class="me-1" for="rank">
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+            </label>
+            <input type="checkbox" id="rank" name="rank" value=">=3">
+
+            <input class="d-block mt-3 ms-5" type="submit" value="Filtra">
+        </form>
+
+        <table class="table">
+            <thead>
+                <tr class="text-uppercase">
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance</th>
+                </tr>
+            </thead>
     
-    <table class="container table mt-5">
-        <thead>
-            <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Description</th>
-                <th scope="col">Parking</th>
-                <th scope="col">Vote</th>
-                <th scope="col">Distance</th>
-            </tr>
-        </thead>
-
-        <tbody>
-            <?php 
-
-            foreach ($hotels as $currentHotel) {
-                echo "<tr>";
-
-                foreach ($currentHotel as $key => $value) {
-                    if ($key == 'parking') {
-                        echo "<td>" . ($value ? 'Yes' : 'No') . "</td>";
-                    } else {
-                        echo "<td>" . $value . "</td>";
+            <tbody>
+                <?php 
+    
+                foreach ($hotels as $currentHotel) {
+                    echo "<tr>";
+    
+                    foreach ($currentHotel as $key => $value) {
+                        if ($key == 'parking') {
+                            echo "<td>" . ($value ? 'Yes' : 'No') . "</td>";
+                        } else {
+                            echo "<td>" . $value . "</td>";
+                        }
                     }
+    
+                    echo "</tr>";
                 }
-                
-                echo "</tr>";
-            }
-            ?>
-        </tbody>
-
-    </table>
+                ?>
+            </tbody>
+    
+        </table>
+    </div>
 
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
